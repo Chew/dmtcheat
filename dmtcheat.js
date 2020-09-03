16c01d2174c7cc52123ec9aabf89aaa7d6d4dd27
Chew <chew@chew.pw>
2020-09-03 00:54:18 -0500

    IntelliJ Suggestions

--- dmtcheat.js
+++ dmtcheat.js
@@ -1,4 +1,4 @@
-var servers = [
+const servers = [
     {
         name: "Mineplex",
         game: "Draw My Thing",
@@ -28,7 +28,7 @@ var servers = [
         defaultWordLength: [[4], [4, 4], [3, 3, 3]]
     }
     // You can add more servers if you want, they will appear in the list automatically
-]
+];
 
 const DEFAULT_SERVER_INDEX = 0;
 const MAXIMUM_POSSIBLE_LENGTH = 20; // Excluding spaces, no word must be bigger than this (maximum number of hint inputs)
@@ -38,34 +38,36 @@ const MAX_WORDS_SHOWN = 20;
 const SPLIT_WORDS_COLUMN_AT = 10;
 
 let selectedServer = servers[DEFAULT_SERVER_INDEX];
-var wordCount;
-var wordLength;
+let wordCount;
+let wordLength;
 
-var rawWordLength;
-var spaceIndexes;
-var matchWords;
-var allWordsShown;
-var copiedWords;
-var currentHint;
+let rawWordLength;
+let spaceIndexes;
+let matchWords;
+let allWordsShown;
+let copiedWords;
+let currentHint;
 
-var wordCountRadio;
-var wordLengthSpinner;
-var hintInput;
-var listLeftColumn;
-var listRightColumn
-var showAllBtn;
-var resetCopyBtn;
-var clearInputBtn;
-var missingWordDiv;
+let wordCountRadio;
+let wordLengthSpinner;
+let hintInput;
+let listLeftColumn;
+let listRightColumn;
+let showAllBtn;
+let resetCopyBtn;
+let clearInputBtn;
+let missingWordDiv;
 
 /**
  Load word lists from all server and find max word count,
  min/max possible lengths for each word count
  */
 function loadWordLists() {
-    for (var s = 0; s < servers.length; s++) {
-        // Load word list for that server
-        var rawFile = new XMLHttpRequest();
+    let j;
+    for (let s = 0; s < servers.length; s++) {
+        let i;
+// Load word list for that server
+        const rawFile = new XMLHttpRequest();
         rawFile.open("GET", servers[s].wordListFile, false);
         rawFile.onreadystatechange = function() {
             if (rawFile.readyState === 4) {
@@ -78,21 +80,21 @@ function loadWordLists() {
 
         // Find max and min
         servers[s].maxWordCount = 1;
-        var max = [];
-        var min = [];
-        for (var i = 0; i < 3; i++) {
+        const max = [];
+        const min = [];
+        for (i = 0; i < 3; i++) {
             max.push([]);
             min.push([]);
-            for (var j = 0; j <= i; j++) {
+            for (j = 0; j <= i; j++) {
                 max[i].push(0);
                 min[i].push(100);
             }
         }
 
-        var word;
-        var parts;
-        var wordCount;
-        for (var i = 0; i < servers[s].wordList.length; i++) {
+        let word;
+        let parts;
+        let wordCount;
+        for (i = 0; i < servers[s].wordList.length; i++) {
             word = servers[s].wordList[i];
             if (!servers[s].allowNumbers && /[0-9]/.test(word)) {
                 servers[s].allowNumbers = true;
@@ -103,8 +105,8 @@ function loadWordLists() {
             parts = word.split(" ");
             wordCount = parts.length;
             if (wordCount > servers[s].maxWordCount) servers[s].maxWordCount = wordCount;
-            for (var j = 0; j < wordCount; j++) {
-                var length = parts[j].length;
+            for (j = 0; j < wordCount; j++) {
+                const length = parts[j].length;
                 if (length < min[wordCount-1][j]) min[wordCount-1][j] = length
                 if (length > max[wordCount-1][j]) max[wordCount-1][j] = length
             }
@@ -135,9 +137,9 @@ function onServerSelected(server) {
     wordCountRadio[wordCount-1].checked = true;
     onWordCountChanged(wordCount);
 
-    if (server.maxWordCount == 1) {
+    if (server.maxWordCount === 1) {
         document.getElementById("word-count-div").style.display = "none";
-    } else if (server.maxWordCount == 2) {
+    } else if (server.maxWordCount === 2) {
         document.getElementById("word-count-div").style.display = "block";
         document.getElementById("word-count-2").style.display = "inline";
         document.getElementById("word-count-3").style.display = "none";
@@ -155,18 +157,18 @@ function onServerSelected(server) {
 function onWordCountChanged(count) {
 
     wordLength = selectedServer.defaultWordLength[count-1].slice(0);
-    for (var i = 0; i < (count == 1 ? 1 : count+1); i++) {
-        var word = (i <= 1 ? 0 : i-1);
+    for (let i = 0; i < (count === 1 ? 1 : count+1); i++) {
+        const word = (i <= 1 ? 0 : i - 1);
         wordLengthSpinner[i].min = selectedServer.minWordLength[count-1][word];
         wordLengthSpinner[i].max = selectedServer.maxWordLength[count-1][word];
         wordLengthSpinner[i].value = wordLength[word];
     }
     onWordLengthChanged();
 
-    if (count == 1) {
+    if (count === 1) {
         document.getElementById("single-word-length-div").style.display = "block";
         document.getElementById("multiple-word-length-div").style.display = "none";
-    } else if (count == 2) {
+    } else if (count === 2) {
         document.getElementById("single-word-length-div").style.display = "none";
         document.getElementById("multiple-word-length-div").style.display = "block";
         document.getElementById("word-length-label-3").style.display = "none";
@@ -179,21 +181,21 @@ function onWordCountChanged(count) {
 
 /**
  Called when word length spinner value is changed
- @param length New length of the word
  */
 function onWordLengthChanged() {
-    // Adjust hint inputs to match words lengths
+    let i;
+// Adjust hint inputs to match words lengths
     rawWordLength = 0;
     spaceIndexes = [];
-    for (var i = 0; i < wordCount; i++) {
+    for (i = 0; i < wordCount; i++) {
         rawWordLength += wordLength[i];
-        if (wordCount != 1 && i < wordCount-1) spaceIndexes.push(rawWordLength);
+        if (wordCount !== 1 && i < wordCount-1) spaceIndexes.push(rawWordLength);
     }
-    var spaces = spaceIndexes.slice(0);
-    for (var i = 0; i < MAXIMUM_POSSIBLE_LENGTH; i++) {
+    const spaces = spaceIndexes.slice(0);
+    for (i = 0; i < MAXIMUM_POSSIBLE_LENGTH; i++) {
         hintInput[i].style.display = (i < rawWordLength ? "inline" : "none");
         hintInput[i].value = "";
-        if (spaces.length > 0 && i == spaces[0]) {
+        if (spaces.length > 0 && i === spaces[0]) {
             // Add left margin to add space between words
             hintInput[i].style.marginLeft = HINT_SPACE_WIDTH;
             spaces.shift();
@@ -222,17 +224,18 @@ function changeCurrentHint(dir) {
  Update word list to match the hints
  */
 function findWords() {
-    // Create template matching the hints -> dr_w _y th_ng
-    var template = "";
-    var spaces = spaceIndexes.slice(0);
-    var showClear = false
-    for (var i = 0; i < rawWordLength; i++) {
-        if (spaces.length > 0 && i == spaces[0]) {
+    let i;
+// Create template matching the hints -> dr_w _y th_ng
+    let template = "";
+    const spaces = spaceIndexes.slice(0);
+    let showClear = false;
+    for (i = 0; i < rawWordLength; i++) {
+        if (spaces.length > 0 && i === spaces[0]) {
             template += " ";
             spaces.shift();
         }
-        var val = hintInput[i].value;
-        if (val.length == 0) {
+        const val = hintInput[i].value;
+        if (val.length === 0) {
             template += "_";
         } else {
             template += val.toLowerCase();
@@ -243,13 +246,13 @@ function findWords() {
 
     // Find words in list matching template
     matchWords = [];
-    for (var i = 0; i < selectedServer.wordList.length; i++) {
-        var word = selectedServer.wordList[i];
-        if (template.length == word.length) {
-            var match = true;
-            for (var j = 0; j < word.length; j++) {
-                var char = template.charAt(j);
-                if (!(char == "_" && word.charAt(j) != " " || char == word.charAt(j))) {
+    for (i = 0; i < selectedServer.wordList.length; i++) {
+        const word = selectedServer.wordList[i];
+        if (template.length === word.length) {
+            let match = true;
+            for (let j = 0; j < word.length; j++) {
+                const char = template.charAt(j);
+                if (!(char === "_" && word.charAt(j) !== " " || char === word.charAt(j))) {
                     match = false;
                     break;
                 }
@@ -259,11 +262,11 @@ function findWords() {
     }
 
     // Update word list
-    var found = matchWords.length;
-    if (found == 0) {
+    const found = matchWords.length;
+    if (found === 0) {
         document.getElementById("word-found-label").innerHTML = "No matches found";
         missingWordDiv.style.display = "block";
-    } else if (found == 1) {
+    } else if (found === 1) {
         document.getElementById("word-found-label").innerHTML = "1 match found";
         missingWordDiv.style.display = "none";
     } else {
@@ -288,7 +291,7 @@ function findWords() {
 }
 
 function addWordToList(list, index) {
-    var word = document.createElement("p");
+    const word = document.createElement("p");
     word.innerHTML = matchWords[index];
     word.className = "word-found";
     word.title = "Click to copy to clipboard";
@@ -302,27 +305,27 @@ function addWordToList(list, index) {
 }
 
 function showFoundWords(showAll) {
-    var found = matchWords.length;
+    const found = matchWords.length;
 
     while (listLeftColumn.hasChildNodes()) listLeftColumn.removeChild(listLeftColumn.lastChild);
     while (listRightColumn.hasChildNodes()) listRightColumn.removeChild(listRightColumn.lastChild);
 
-    if (found == 0) {
+    if (found === 0) {
         listLeftColumn.style.display = "none";
         listRightColumn.style.display = "none";
     } else if (found < SPLIT_WORDS_COLUMN_AT) {
-        for (i = 0; i < found; i++) {
+        for (let i = 0; i < found; i++) {
             addWordToList(listLeftColumn, i);
         }
         listLeftColumn.style.display = "inline-block";
         listRightColumn.style.display = "none";
     } else {
-        var all = (showAll ? found : Math.min(found, MAX_WORDS_SHOWN));
-        var half = Math.ceil(all/2);
-        for (i = 0; i < half; i++) {
+        const all = (showAll ? found : Math.min(found, MAX_WORDS_SHOWN));
+        const half = Math.ceil(all / 2);
+        for (let i = 0; i < half; i++) {
             addWordToList(listLeftColumn, i);
         }
-        for (i = half; i < all; i++) {
+        for (let i = half; i < all; i++) {
             addWordToList(listRightColumn, i);
         }
         listLeftColumn.style.display = "inline-block";
@@ -333,10 +336,10 @@ function showFoundWords(showAll) {
 function copyToClipboard(text) {
     if (window.clipboardData && window.clipboardData.setData) {
         // IE specific code path to prevent textarea being shown while dialog is visible.
-        return clipboardData.setData("Text", text);
+        return window.clipboardData.setData("Text", text);
 
     } else if (document.queryCommandSupported && document.queryCommandSupported("copy")) {
-        var textarea = document.createElement("textarea");
+        const textarea = document.createElement("textarea");
         textarea.textContent = text;
         textarea.style.position = "fixed";  // Prevent scrolling to bottom of page in MS Edge.
         document.body.appendChild(textarea);
@@ -353,13 +356,14 @@ function copyToClipboard(text) {
 }
 
 function main() {
-    // Load server word lists
+    let i;
+// Load server word lists
     loadWordLists();
 
     // Server dropdown, add servers to list
-    var serverDropdown = document.getElementById("server-dropdown");
-    for (var i = 0; i < servers.length; i++) {
-        var option = document.createElement("option");
+    const serverDropdown = document.getElementById("server-dropdown");
+    for (i = 0; i < servers.length; i++) {
+        const option = document.createElement("option");
         option.text = servers[i].name + " - " + servers[i].game;
         serverDropdown.add(option, i);
     }
@@ -369,7 +373,7 @@ function main() {
 
     // Word count radios
     wordCountRadio = document.querySelectorAll("input[name=word-count]");
-    for (var i = 0; i < 3; i++) {
+    for (i = 0; i < 3; i++) {
         wordCountRadio[i].addEventListener("change", (function(j) {
             return function() {
                 wordCount = j+1
@@ -380,7 +384,7 @@ function main() {
 
     // Word length spinners
     wordLengthSpinner = document.querySelectorAll("input[name=word-length]");
-    for (var i = 0; i < 4; i++) {
+    for (i = 0; i < 4; i++) {
         (function(word) {
             wordLengthSpinner[i].addEventListener("focusin", function() {
                 this.oldValue = this.value;
@@ -406,9 +410,9 @@ function main() {
 
     // Hint inputs
     hintInput = [];
-    var hintDiv = document.getElementById("hint-div");
-    for (var i = 0; i < MAXIMUM_POSSIBLE_LENGTH; i++) {
-        var input = document.createElement("input");
+    const hintDiv = document.getElementById("hint-div");
+    for (i = 0; i < MAXIMUM_POSSIBLE_LENGTH; i++) {
+        const input = document.createElement("input");
         input.className = "input-hint";
         input.style.display = "none";
         input.addEventListener("input", function() {
@@ -430,17 +434,17 @@ function main() {
             if (currentHint < rawWordLength-1) changeCurrentHint(1);
         });
         input.addEventListener("keydown", function(event) {
-            if (event.keyCode == 37) {  // Left
+            if (event.keyCode === 37) {  // Left
                 changeCurrentHint(-1);
-            } else if (event.keyCode == 39) {  // Right
+            } else if (event.keyCode === 39) {  // Right
                 changeCurrentHint(1);
-            } else if (event.keyCode == 46 || event.keyCode == 8) {  // Delete or Backspace
+            } else if (event.keyCode === 46 || event.keyCode === 8) {  // Delete or Backspace
                 this.value = "";
                 findWords();
             }
         });
         (function(j) {
-            input.addEventListener("focus", function(event) {
+            input.addEventListener("focus", function() {
                 currentHint = j;
             });
         })(i);
@@ -459,9 +463,9 @@ function main() {
             allWordsShown = true;
             showFoundWords(true);
 
-            var half = Math.ceil(matchWords.length/2);
-            for (var i = 0; i < copiedWords.length; i++) {
-                var index = copiedWords[i];
+            const half = Math.ceil(matchWords.length / 2);
+            for (let i = 0; i < copiedWords.length; i++) {
+                const index = copiedWords[i];
                 if (index < half) {
                     listLeftColumn.children[index].className += " word-found-copied";
                 } else {
@@ -476,10 +480,11 @@ function main() {
     // Reset copied words button
     resetCopyBtn = document.getElementById("reset-copy-btn");
     resetCopyBtn.addEventListener("click", function () {
-        for (var i = 0; i < listLeftColumn.children.length; i++) {
+        let i;
+        for (i = 0; i < listLeftColumn.children.length; i++) {
             listLeftColumn.children[i].className = "word-found";
         }
-        for (var i = 0; i < listRightColumn.children.length; i++) {
+        for (i = 0; i < listRightColumn.children.length; i++) {
             listRightColumn.children[i].className = "word-found";
         }
         copiedWords = [];
@@ -491,7 +496,7 @@ function main() {
     clearInputBtn.style.visibility = "hidden";
     clearInputBtn.addEventListener("click", function() {
         clearInputBtn.style.visibility = "hidden";
-        for (var i = 0; i < MAXIMUM_POSSIBLE_LENGTH; i++) {
+        for (let i = 0; i < MAXIMUM_POSSIBLE_LENGTH; i++) {
             hintInput[i].value = "";
         }
         findWords();
@@ -502,9 +507,9 @@ function main() {
     missingWordDiv.style.display = "none";
 
     // Word list modal
-    var wordListModal = document.getElementById("word-list-modal");
-    var wordListBtn = document.getElementById("word-list-btn");
-    var wordListCloseBtn = document.getElementById("word-list-modal-close");
+    const wordListModal = document.getElementById("word-list-modal");
+    const wordListBtn = document.getElementById("word-list-btn");
+    const wordListCloseBtn = document.getElementById("word-list-modal-close");
 
     wordListBtn.addEventListener("click", function() {
         wordListModal.style.display = "block";
@@ -514,9 +519,9 @@ function main() {
     });
 
     // Help modal
-    var helpModal = document.getElementById("help-modal");
-    var helpBtn = document.getElementById("help-btn");
-    var helpCloseBtn = document.getElementById("help-modal-close");
+    const helpModal = document.getElementById("help-modal");
+    const helpBtn = document.getElementById("help-btn");
+    const helpCloseBtn = document.getElementById("help-modal-close");
 
     helpBtn.addEventListener("click", function() {
         helpModal.style.display = "block";
@@ -527,9 +532,9 @@ function main() {
 
     window.addEventListener("click", function(event) {
         // Close modals when clicked outside
-        if (event.target == wordListModal) {
+        if (event.target === wordListModal) {
             wordListModal.style.display = "none";
-        } else if (event.target == helpModal) {
+        } else if (event.target === helpModal) {
             helpModal.style.display = "none";
         }
     });
